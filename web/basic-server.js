1a4732eba5530fcde50a5b6b280a7fea421e8f20
Mad Bernard <madeline.a.bernard@gmail.com>
2015-12-16 15:12:57 -0800

    first commit, beginning to add comments, sites.txt initialized

--- web/basic-server.js
+++ web/basic-server.js
@@ -10,6 +10,10 @@ var port = 8080;
 var ip = "127.0.0.1";
 var server = http.createServer(handler.handleRequest);
 
+// this is explained a bit in here:
+// http://stackoverflow.com/questions/13651945/what-is-the-use-of-module-parent-in-node-js-how-can-i-refer-to-the-requireing
+// "The "parent" is the module that caused the script to be interpreted (and cached), if any:
+// so the line below checks if something else called for the basic-server module
 if (module.parent) {
   module.exports = server;
 } else {
