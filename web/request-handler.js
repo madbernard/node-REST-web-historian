9193db5f340e9f8d7c9779af356c88edb3a8ac1d
Mad Bernard <madeline.a.bernard@gmail.com>
2015-12-17 13:55:04 -0800

    before attemtped beautify

--- web/request-handler.js
+++ web/request-handler.js
@@ -50,54 +50,53 @@ exports.handleRequest = function (req, res) {
   }
 
   if(req.method === 'POST'){
-  //   var collectedData = "";
-  //   // console.log(collectedData, "in POST, first appearance of collectedData");
-  //   //302 for found
+    var collectedData = "";
+    // console.log(collectedData, "in POST, first appearance of collectedData");
+    //302 for found
     statusCode = 302;
-  //   req.on("data", function(chunk){
-  //     collectedData += chunk;
-  //   });
-  //   req.on("end", function(){
-  //     // probably we need to store only the url, not url=theurl.com <--DO THIS
-
-  //     console.log(collectedData, "in POST, where collectedData should be complete");
-  //     // check if URL is in archived list (text or sites?) <--DO THIS
-
-  //       // if not,
-  //         // POST process here will call out to that URL <--how done?
-  //         // HTTP-REQUEST (GET) <--how done?
-  //         // async
-  //           // once that returns, add to /sites/ <--maybe we can figure this out?
-  //           // we use fs to store it on our hard drive
-  //         // add url to txt file <--OK
-  //         archive.addUrlToList(collectedData);
-  //         // meanwhile it returns the person the "loading.html" page <--DO THIS
-
-  //       //if URL is in list (text) <--DO THIS
-
-  //         // check that URL is in /sites/ <--maybe we can figure this out?
-
-  //           // if URL is in /sites/
-  //             // we put the person on our version of that URL:
-  //             // we serve them sites/storedthing.html
-  //         // if URL is not yet downloaded <--maybe we can figure this out?
-
-  //           // loading page is served to person <--DO THIS
-
-  //     res.writeHead(statusCode, "302 found", httpHelpers.headers);
-  //     res.end();
-  //   });
-  // }
-// require the basic.server.js, then we can use this
-    httpr.get('https://www.google.com/', function (err, res) {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      console.log(res.code, res.headers, res.buffer.toString());
+    req.on("data", function(chunk){
+      collectedData += chunk;
     });
-        res.writeHead(statusCode, "302 found", httpHelpers.headers);
-        res.end();
+    req.on("end", function(){
+     // probably we need to store only the url, not url=theurl.com <--DO THIS
+
+      console.log(collectedData, "in POST, where collectedData should be complete");
+      // check if URL is in archived list (text or sites?) <--DO THIS
+
+        // if not,
+          // POST process here will call out to that URL <--how done?
+          // HTTP-REQUEST (GET) <--how done?
+          // async
+          httpr.get('https://www.google.com/', function (err, res) {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            console.log(res.code, res.headers, res.buffer.toString());
+          });
+            // once that returns, add to /sites/ <--maybe we can figure this out?
+            // we use fs to store it on our hard drive
+          // add url to txt file <--OK
+          archive.addUrlToList(collectedData);
+          // meanwhile it returns the person the "loading.html" page <--DO THIS
+
+        //if URL is in list (text) <--DO THIS
+
+          // check that URL is in /sites/ <--maybe we can figure this out?
+
+            // if URL is in /sites/
+              // we put the person on our version of that URL:
+              // we serve them sites/storedthing.html
+          // if URL is not yet downloaded <--maybe we can figure this out?
+
+            // loading page is served to person <--DO THIS
+
+      res.writeHead(statusCode, "302 found", httpHelpers.headers);
+      res.end();
+    });
+  }
+        // res.writeHead(statusCode, "302 found", httpHelpers.headers);
+        // res.end();
  }
 //npm install http-request --save
 
